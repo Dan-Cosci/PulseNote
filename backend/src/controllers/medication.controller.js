7e145e4d0a1001708ba3faedb108cf4c6206d396
Daniel <allinasdaniel@gmail.com>
2026-01-16 14:55:09 +0800

    medication route

--- backend/src/controllers/medication.controller.js
+++ backend/src/controllers/medication.controller.js
@@ -88,7 +88,67 @@ export const createMedication = asyncHandler(async (req, res) =>{
   })
 });
 
-export const updateMedication = asyncHandler(async (req, res) =>{});
-export const alterMedication = asyncHandler(async (req, res) =>{});
+export const updateMedication = asyncHandler(async (req, res) =>{
+  const { id } = req.params;
+  const { medicine, dosage, frequency } = req.body;
 
-export const deleteMedication = asyncHandler(async (req, res) =>{});
+  if (!medicine || !dosage || !frequency) {
+    throw new AppError("Medicine, dosage and frequency are required", 400);
+  }
+
+  const updatedMed = await Medication.findByIdAndUpdate(
+    id,
+    { medicine, dosage, frequency },
+    { new: true, runValidators: true }
+  );
+
+  if (!updatedMed) {
+    throw new AppError("Medication not found", 404);
+  } 
+
+  res.status(200).json({
+    success: true,
+    message: "Medication updated successfully",
+    data: updatedMed
+  });
+
+});
+
+export const alterMedication = asyncHandler(async (req, res) =>{
+  const { id } = req.params;
+  const { field, value } = req.body;
+
+  const updatedMed = await Medication.findByIdAndUpdate(
+    id,
+    { [field]: value },
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).json({
+    success: true,
+    message: "Medication altered successfully",
+    data: updatedMed
+  });
+});
+
+// DELETE METHODS
+export const deleteMedication = asyncHandler(async (req, res) =>{
+  const { id } = req.params;
+  const med = await Medication.findById(id);
+  if (!med) {
+    throw new AppError("Medication not found", 404);
+  }
+  if (med.deletedAt){
+    throw new AppError("Medication already deleted", 400);
+  }
+
+  med.deletedAt = new Date();
+  await med.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Medication deleted successfully",
+    data: med
+  });
+
+});
