aeb1dd5f4a0ae83009d43b59bb1ddd46674e1da1
Daniel <allinasdaniel@gmail.com>
2026-01-16 15:52:53 +0800

    started frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1 className=''>Hello world!</h1>
-    </div>
+    <div>App</div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import "./assets/css/style.css"
+
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Toaster 
+        position="top-center"
+        reverseOrder={false}
+      />
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
