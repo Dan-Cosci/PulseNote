6d5f1bbd5b26c7b9d7a9ac58605bc579b5f1d7e0
Daniel <allinasdaniel@gmail.com>
2026-02-21 21:15:04 +0800

    implimented basic router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
+import { RouterProvider } from 'react-router-dom'
 
-import App from './App.jsx'
+import Router from './routes/Router'
 import './assets/css/main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Toaster />
-    <App />
+    <RouterProvider router={Router} />
   </StrictMode>,
 )
